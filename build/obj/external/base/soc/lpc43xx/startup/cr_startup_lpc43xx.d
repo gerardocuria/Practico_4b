fe59cbb422398bffa480ed3374b244c941cccf5e
Gerardo <gerardocuria193@gmail.com>
2023-04-28 18:55:15 -0300

    Primer Version

---
+++ build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.d
@@ -0,0 +1,2 @@
+build/obj/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.o: \
+ /home/gerardo/Facultad/Sistemas_Embebidos/Practico_4/plantilla-embebidos/muju/external/base/soc/lpc43xx/startup/cr_startup_lpc43xx.c
